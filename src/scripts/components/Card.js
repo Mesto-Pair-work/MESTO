f469d1c5acf2469a95a0d711cff545009ddd090b
evlukas <evlukas@mail.ru>
2022-11-08 19:57:56 +0400

    Добавил классы Api, Card, Section, Userinfo

---
+++ src/scripts/components/Card.js
@@ -0,0 +1,105 @@
+"use strict";
+
+//Класс принимает в конструктор данные карты(Card) и селектор её template-элемента (selector);
+export class Card {
+  constructor(selector, card, api, handleCardClick) {
+    this._selector = selector;
+    this._link = card.link;
+    this._title = card.name;
+    this._likes = card.likes;
+    this._userId = card.userId;
+    this._id = card._id;
+    this._owner = card.owner;
+    this._api = api;
+    this._handleCardClick = handleCardClick;
+  }
+  
+
+  _getElement() {
+    const cardElement = document
+      .querySelector(this._selector)
+      .content.querySelector(".places-box")
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  _handleAddLike() {
+    if (!this._boxLike.classList.contains("places-box__like_active")) {
+      this._api.addLike(this._id) 
+        .then((card) => {
+          this._boxLike.classList.toggle("places-box__like_active");
+          this._placesBboxLikeNum.textContent = card.likes.length;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      this._api.delLike(this._id) 
+        .then((card) => {
+          this._boxLike.classList.toggle("places-box__like_active");
+          this._placesBboxLikeNum.textContent = card.likes.length;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  _showDelIcon() {
+    //Если id-карты и id-юзера совпадают, то показываем иконку корзины для удаления карточки
+    if (this._owner._id !== this._userId) {
+      this._iconDelCard.remove();
+    } else {
+      this._iconDelCard.addEventListener("click", () => {
+        this._api.delCard(this._id)
+          .then((card) => {
+            this._element.remove();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    }
+  }
+
+  _isLike() {
+    //Если в массиве лайков есть мой лайк, то делаем сердечко лайкнутым
+    if (this._likes.find((like) => like._id === this._userId)) {
+      this._boxLike.classList.add("places-box__like_active");
+    }
+  }
+
+  _setEventListeners() {
+    this._placesBoxPhoto.addEventListener("click", () => {
+      this._handleCardClick(this._link, this._title);
+    });
+
+    this._boxLike.addEventListener("click", () => {
+      this._handleAddLike();
+    });
+  }
+
+   //Создаем элемент, навешиваем события, заполняем данными, реализуем логику лайков
+   generate() {
+    this._element = this._getElement();
+    this._iconDelCard = this._element.querySelector(".places-box__del-card");
+    this._placesBoxPhoto = this._element.querySelector(".places-box__photo");
+    this._placesBoxName = this._element.querySelector(".places-box__name");
+    this._placesBboxLikeNum = this._element.querySelector(
+      ".places-box__like-num"
+    );
+    this._boxLike = this._element.querySelector(".places-box__like");
+    this._placesBoxPhoto.src = this._link;
+    this._placesBoxPhoto.alt = this._title;
+    this._placesBoxName.textContent = this._title;
+    this._placesBboxLikeNum.textContent = this._likes.length;
+
+    this._isLike();
+    this._showDelIcon();
+    this._setEventListeners();
+
+    return this._element;
+  }
+
+}
---
+++ src/scripts/components/Userinfo.js
@@ -0,0 +1,40 @@
+export class UserInfo {
+  constructor(fullNameSelector, occupationSelector, avatarSelector, api) {
+    this._aboutElement = document.querySelector(occupationSelector);
+    this._nameElement = document.querySelector(fullNameSelector);
+    this._avatarElement = document.querySelector(avatarSelector);
+    this._api = api;
+  }
+
+  getUserInfo() {
+    return this._api.getUser().catch((err) => {
+      console.log(err);
+    });
+  }
+
+  setUserInfo(user, ava = false) {
+    return this._api
+      .editUser(user)
+      .then((user) => {
+        this._aboutElement.textContent = user.about;
+        this._nameElement.textContent = user.name;
+        if (ava) {
+          this.setAvatarInfo(user.avatar);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  setAvatarInfo(url) {
+    return this._api
+      .editAvatar(url)
+      .then((user) => {
+        this._avatarElement.src = user.avatar;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
